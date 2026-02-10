fc96da376e7c8e1175f141dc03d4b18addecff19
DIENIFER DA COSTA OLIVEIRA <dienifer.oliveira@senaisp.edu.br>
2026-02-10 11:26:44 -0300

    introdução javascrpts

---
+++ script.js
@@ -0,0 +1,99 @@
+var a = 1;
+let b = 2;
+const c = 3;
+
+console.log(a +1);
+console.log(b + 1);
+console.log(c + 1);
+
+//alert("dienifer fofa"); 
+
+/*const nome = prompt("qual seu nome?")
+console.log(nome); */
+
+
+//console.log(confirm("você é real?"))
+
+
+//execicios
+
+/*const nome = prompt("qual é seu nome?")
+console.log ("ola" + nome);*/  
+
+/*let materia = prompt("Qual sua disciplina favorita?");
+alert(materia);
+console.log(`A matéria ${materia} é muito boa`);*/
+
+
+
+/*const data = prompt("qual a data de hoje?")
+console.log(data);*/
+
+/*console.log(confirm("Está preparado para segundo semestre de desenvovimenro de sistema?"))*/
+
+
+
+a = 3;
+
+console.log(a++)
+console.log(a--)
+
+4 > 3 && 5 > 6
+
+
+/*const num1 = Number(prompt("Insira um número:"));
+const num2 = Number(prompt("Insira outro número:"));
+alert(`A soma é: ${num1 + num2}`);
+alert(`A multiplicação é: ${num1 * num2}`)
+alert(`A subtração é: ${num1 - num2}`)
+alert(`A divisão é: ${num1 / num2}`)*/
+
+
+/*const nota1 = Number(prompt("Insira uma nota:"));
+const nota2 = Number(prompt("Insira outra nota:"));
+const nota3 = Number(prompt("Insira mais uma nota:"));
+
+const media = (nota1 + nota2 + nota3) / 3;
+
+alert(`A média das suas notas é: ${media}`);*/
+
+
+/*const largura = Number(prompt("qual a largura:"));
+const altura = Number(prompt("qual a altura:"));
+
+const area = (largura * altura)
+const litros = (area/2)
+
+console.log(`Área: ${area}, Litros: ${litros}`);*/
+
+
+
+let maior =  18
+if (maior > 19 )console.log("maior!")
+else console.log("menor!")
+
+let cor = prompt("cor favorita");
+
+switch(cor){
+    case "preto":
+        console.log("você é a koromi")
+        break;
+    case "roxo":
+        console.log("você é my melody")
+        break;
+    case "branco":
+        console.log("você é my chinamonroll")
+        break;
+    case "pink":
+        console.log("você é my hellokitte")
+        break;
+    case "azul":
+        console.log("você é luxo")
+        break;
+    case "verde":
+        console.log("você é o  keroppi")
+        break;
+    case "amarelo":
+        console.log("você é o  keroppi")
+        break;
+}
